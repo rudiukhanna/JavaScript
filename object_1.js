b7113bb8282629225b583f7b97774ecb01b8751e
Rudiuk Hanna <melomanna@gmail.com>
2023-03-07 15:58:35 +0200

    h/w_25

---
+++ object_1.js
@@ -0,0 +1,15 @@
+const potions = [
+    { name: "Speed potion", price: 460 },
+    { name: "Dragon breath", price: 780 },
+    {},
+    { name: "Stone skin", price: 520 },
+  ]
+
+  const isObjectEmpty = function (obj) {
+    return Object.keys(obj).length === 0;
+  }
+
+  let res = isObjectEmpty({});
+  console.log(res);
+  res = isObjectEmpty({ name: "Stone skin", price: 520 });
+  console.log(res);
