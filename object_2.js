9b3e0b086e2ab59186d96ce3ef2781216ba58ab3
Rudiuk Hanna <melomanna@gmail.com>
2023-03-11 20:55:03 +0200

    H/w 25

--- object_2.js
+++ object_2.js
@@ -1,9 +1,19 @@
 const user = {
-    name: 'Ганна',
-    age: 32,
+    name: prompt('Як тебе звати?'),
+    age: prompt('Скільки тобі років?', 0),
     sayHello() {
-        console.log('Привіт, я ' + this.name + ', ' + 'мені ' + this.age + ' роки.');
-    },
+        if(this.age % 100 >= 5 && this.age % 100 <= 20) {
+            alert(message = `Привіт, я ${this.name}, мені ${this.age} років`);
+        } else {
+             if (this.age % 10 == 1) {
+                alert(message = `Привіт, я ${this.name}, мені ${this.age} рік`);
+             } else if ( this.age % 10 >= 2 && this.age % 10 <= 4) {
+                alert(message = `Привіт, я ${this.name}, мені ${this.age} роки`);
+             } else {
+                alert(message = `Привіт, я ${this.name}, мені ${this.age} років`)
+             }
+        }
+    }
 }
 
 user.sayHello();
