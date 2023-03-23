26d0bce7ccbc2189dd536d60dca0a07944a1877d
Rudiuk Hanna <melomanna@gmail.com>
2023-03-23 15:55:59 +0200

    min and max functions were updated

--- js/func5.js
+++ js/func5.js
@@ -1,14 +1,16 @@
-function getSmallestElem(arr) {
-    
-        let smallestElem = arr[0];
+function findMin(arr) {
+    let min = undefined;
 
-        for (let elem of arr){
-            if (elem < smallestElem) {
-                smallestElem = elem;
-            }
+    for (let elem of arr) {
+        if (typeof elem === 'number' && (min === undefined || elem < min)) {
+            min = elem;
         }
-        
-        return smallestElem;
+    }
+    return min;
 }
 
-console.log(getSmallestElem([2, 17, 53, 15, 105, 73, -7, 12, 0]))
+
+console.log(findMin([2, 17, 53, 'cat', 15, 105, 73, 'hello', -7, 12, 0]));
+console.log(findMin([ 'cat',  'hello' ]));
+console.log(findMin([ 'cat',  'hello', 'js', 'is', 'awesome' ]));
+console.log(findMin([ 2, 15, 75, 0 ]));
--- js/func6.js
+++ js/func6.js
@@ -1,13 +1,14 @@
-function getBiggestElem(arr) {
-    let biggestElem = arr[0];
+function findMax(arr) {
+    let max = undefined;
 
     for (let i = 0; i < arr.length; i++) {
-      if ( arr[i] >  biggestElem) {
-        biggestElem = arr[i];
-      }
-    }
-
-    return biggestElem;
+     if(typeof arr[i] === 'number' && ( max === undefined || arr[i] > max)) {
+      max = arr[i];
+     }
+}
+   return max;
 }
 
-console.log(getBiggestElem([2, 17, 53, 15, 105, 73, -7, 12, 0])) 
+console.log(findMax([2, 17, 53, 'cat', 15, 105, 73, 'hello', -7, 12, 0])) 
+console.log(findMax([ 'cat','hello' ])) 
+console.log(findMax([ 'cat','hello', 'js', 'is', 'awesome', 0]));
