323538dfa4e954787984c9635fcc87658192bb35
Rudiuk Hanna <melomanna@gmail.com>
2023-03-23 14:37:18 +0200

    array's elements were added

--- js/max.js
+++ js/max.js
@@ -1,9 +1,8 @@
 // Пошук максимального значення елемента масиву
 
-const arr2 = [2, 17, 53, 15, 105, 73, -7, 12, 0, 27];
+const arr2 = [2, 17, 'hello', 53, 15, 105, 'cat', 73, -7, 12, 0, 'js', 27];
 
-let biggestElem = Number.isNaN(arr2[0]);
-
+let biggestElem = Number.isNaN(arr2[0]); 
 for (let i = 0; i < arr2.length; i++) {
     if (arr2[i] > biggestElem) {
         biggestElem = arr2[i];
