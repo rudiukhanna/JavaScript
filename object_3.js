3084befbce22371a3bf8b9147bfb1b36d703fd02
Rudiuk Hanna <melomanna@gmail.com>
2023-03-26 00:25:18 +0200

    update after h/w review

--- object_3.js
+++ object_3.js
@@ -2,15 +2,27 @@ const calc = {
     num1: 0,
     num2: 0,
     ask() {
-     this.num1 = Number(prompt('Введіть перше число', 0));
-     this.num2 = Number(prompt('Введіть друге число', 0));
+     this.num1 = Number(prompt('Введіть перше число'));
+     this.num2 = Number(prompt('Введіть друге число'));
     },
-    sum() {
-    alert( 'Сума введених чисел: ' + (this.num1 + this.num2)); 
-    },
+     sum() { 
+            if(isNaN(this.num1) || isNaN(this.num2)) {
+                alert('Введені дані не є числами!')
+            } else if(this.num1 == '' || this.num2 == '') {
+               alert('Поле не може бути порожнім!');
+            } else {
+                alert( 'Сума введених чисел: ' + (this.num1 + this.num2));
+                }
+                 },
     mul() {
-     alert( 'Добуток введених чисел: ' + this.num1 * this.num2);
-    },
+            if(isNaN(this.num1) || isNaN(this.num2)) {
+                alert('Введені дані не є числами!')
+            } else if(this.num1 == '' || this.num2 == '') {
+                alert('Поле не може бути порожнім!');
+            } else {
+        alert( 'Добуток введених чисел: ' + this.num1 * this.num2);
+            };
+         }, 
 }
 
 calc.ask()
