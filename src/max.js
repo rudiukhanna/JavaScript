f2d06f02f9513b7cc9457c6b3b1fcbd5090df6a4
Rudiuk Hanna <melomanna@gmail.com>
2023-04-06 15:22:30 +0300

    adding function Lesson_23

---
+++ src/max.js
@@ -0,0 +1,43 @@
+const arr2 = [2, 17, 'hello', 53, 15, 105, 'cat', 73, -7, 12, 0, 'js', 27];
+
+let biggestElem = Number.isNaN(arr2[0]); 
+for (let i = 0; i < arr2.length; i++) {
+    if (arr2[i] > biggestElem) {
+        biggestElem = arr2[i];
+    }
+}
+
+
+
+const arr1 = [2, 17, 'hello', 53, 15, 105, 'cat', 73, -7, 12, 0, 'js', 27];
+
+let smallestElem = Number.isNaN(arr1[0]); 
+
+
+for (let elem of arr1){
+   if (elem < smallestElem) {
+        smallestElem = elem;
+   }
+}
+
+console.log(smallestElem);
+
+
+
+const arr = [2, 17, 'hello', 53, 15, 105, 'cat', 73, -7, 12, 0, 'js', 27];
+
+let sum = 0;
+
+for ( let elem of arr ) {
+   if (typeof elem === 'number') {
+    sum += elem;
+   }
+}
+
+
+let str = '';
+
+for (let i = 0; i < 5; i++) {
+    str += '#';
+    console.log(str);
+}
